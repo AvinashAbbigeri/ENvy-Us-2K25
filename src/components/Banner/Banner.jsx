2a4998e972d3648e9ecfd5b6fb35fbb027988268
AvinashAbbigeri <avinashabbigeri40@gmail.com>
2025-04-11 11:14:15 +0530

    update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,6 +10,8 @@ const Banner = () => {
                 <h2 className='poppins font-medium lg:text-[50px] md:text-[40px] text-[32px]'>Apr 16/25</h2>
                 <p className='poppins-regular text-[#000000] lg:text-[16px] md:text-[14px] text-[14px] xl:w-[500px] lg:w-[450px] md:[350px] w-[350px] tracking-wide leading-6 font-medium'>Empowering the students through technology innovation. Join us on a journey of endless possibilities</p>
                 <Link to="https://docs.google.com/forms/d/1zZ1e1FGZHaDgVmbR17X9oC0LYFLXHPS0k4HOp8LKs5Y/edit" className='lg:text-[26px] md:text-[20px] text-[14px] text-[#F5C98A] bg-[#023047] w-max px-3 py-1 rounded-md mt-4 '>Register Now</Link>
+                <Link to="/events" className='lg:text-[26px] md:text-[20px] text-[14px] text-[#F5C98A] bg-[#023047] w-max px-3 py-1 rounded-md mt-4 '>View Events</Link>
+                <Link to="/venue" className='lg:text-[26px] md:text-[20px] text-[14px] text-[#F5C98A] bg-[#023047] w-max px-3 py-1 rounded-md mt-4 '>Venue List</Link>
             </div>
             <div className="absolute md:relative top-[30vh] md:top-0 mt-[5rem] lg:mt-[1rem]">
                 <img className='w-[130vw] md:w-[900px]' src={banner} alt="" />
